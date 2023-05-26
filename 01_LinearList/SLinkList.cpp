0724f7d1b2ef6fc4fc0c37bf89a27deb62460efa
longyan <longyan@nit.zju.edu.cn>
2023-05-26 13:07:21 +0800

    静态链表

---
+++ 01_LinearList/SLinkList.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file SLinkList.cpp
+ * @brief: 静态链表
+ * @author: Long Yan
+ * @date: 2023-05-09
+ *
+ **/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+#define MaxSize 10
+
+typedef struct {
+	int data;
+	int next;
+} SLinkList[MaxSize];
+
+int main() {
+	SLinkList a;
+	printf("Size a = %lld\n", sizeof(a));
+
+	return 0;
+}
