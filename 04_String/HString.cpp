f99ac3318ebc8497c953e1814f31ce77ec5129ff
longyan <longyan@nit.zju.edu.cn>
2023-05-28 01:33:15 +0800

    串的堆分配

---
+++ 04_String/HString.cpp
@@ -0,0 +1,27 @@
+/**
+ * @file: HString
+ * @brief: 堆分配
+ * @author: Long Yan
+ * @date: 2023-05-28
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define MAXLEN 255
+
+typedef struct
+{
+    char *ch;
+    int length;
+} HString;
+
+int main()
+{
+    HString S;
+    S.ch = (char *)malloc(sizeof(char) * MAXLEN);
+    S.length = 0;
+
+    return 0;
+}
