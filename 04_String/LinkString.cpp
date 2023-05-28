7b3c17c5d3643776d1e68a2acee9cccead386b40
longyan <longyan@nit.zju.edu.cn>
2023-05-28 15:51:55 +0800

    串的链式存储

---
+++ 04_String/LinkString.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file: LinkString
+ * @brief: 串的链式存储
+ * @author: Long Yan
+ * @date: 2023-05-28
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct StringNode
+{
+    char ch[4];
+    struct StringNode *next;
+} StringNode, *String;
+
+int main()
+{
+
+    return 0;
+}
