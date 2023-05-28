ab15167b656fefc07852e509523e6ffc701f4a43
longyan <longyan@nit.zju.edu.cn>
2023-05-28 15:26:05 +0800

    朴素模式匹配算法（暴力算法）

---
+++ 04_String/KMP.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file: KMP
+ * @brief: 串的模式匹配算法
+ * @author: Long Yan
+ * @date: 2023-05-28
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+int main(){
+    
+    return 0;
+}
